772fdc1da1a5e0c4f65d8ea1d5da39d4dd8bfa53
ZamzamAlfulaiti <geniusalfulaiti20@gmail.com>
2025-08-02 11:50:52 +0400

    Add solution for warmup1-mixStart

---
+++ warmup1-mixStart/mixStart.java
@@ -0,0 +1,11 @@
+public boolean mixStart(String str) {
+  if (str.length() >= 3){
+    if (str.substring(1, 3).equals("ix")) {
+      return true;
+    } else {
+      return false;
+    }
+  }else{
+    return false;
+  }
+}

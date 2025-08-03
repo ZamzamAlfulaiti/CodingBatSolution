36cd3d600ae04c484f6a22d5a6b19a191bb3240d
ZamzamAlfulaiti <geniusalfulaiti20@gmail.com>
2025-08-03 15:07:34 +0400

    Add solution for logic1-love

---
+++ logic1-love6/love6.java
@@ -0,0 +1,11 @@
+public boolean love6(int a, int b) {
+  if (a == 6 || b == 6) {
+    return true;
+  } else if (a + b == 6) {
+    return true;
+  } else if (Math.abs(a - b) == 6) {
+    return true;
+  } else {
+    return false;
+  }
+}
